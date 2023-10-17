a262508d37f48597e34187ed71b9b4768af042e9
Yerzat <erzat.orazzhan@icloud.com>
2023-10-17 21:35:43 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyFirstAppInsta_HW1"
+include(":app")
